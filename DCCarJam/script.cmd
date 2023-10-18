bde8d313e11485dcf6ac4378c72db20f32e7398c
KAORI KANNO <kaori.kanno@uc.cl>
2023-10-18 10:11:19 -0300

    back todo

---
+++ DCCarJam/script.cmd
@@ -0,0 +1,135 @@
+python game/run.py astar tests/test1.txt h0 2
+python game/run.py astar tests/test1.txt h1 2
+python game/run.py astar tests/test1.txt h2 2
+python game/run.py astar tests/test1.txt h3 2
+
+python game/run.py astar tests/test2.txt h0 2
+python game/run.py astar tests/test2.txt h1 2
+python game/run.py astar tests/test2.txt h2 2
+python game/run.py astar tests/test2.txt h3 2
+
+python game/run.py astar tests/test3.txt h0 2
+python game/run.py astar tests/test3.txt h1 2
+python game/run.py astar tests/test3.txt h2 2
+python game/run.py astar tests/test3.txt h3 2
+
+python game/run.py astar tests/test4.txt h0 1
+python game/run.py astar tests/test4.txt h1 1
+python game/run.py astar tests/test4.txt h2 1
+python game/run.py astar tests/test4.txt h3 1
+
+python game/run.py astar tests/test5.txt h0 1
+python game/run.py astar tests/test5.txt h1 1
+python game/run.py astar tests/test5.txt h2 1
+python game/run.py astar tests/test5.txt h3 1
+
+python game/run.py astar tests/test6.txt h0 1
+python game/run.py astar tests/test6.txt h1 1
+python game/run.py astar tests/test6.txt h2 1
+python game/run.py astar tests/test6.txt h3 1
+
+python game/run.py astar tests/test7.txt h0 1
+python game/run.py astar tests/test7.txt h1 1
+python game/run.py astar tests/test7.txt h2 1
+python game/run.py astar tests/test7.txt h3 1
+
+python game/run.py astar tests/test_anytime.txt h0 1
+python game/run.py astar tests/test_anytime.txt h1 1
+python game/run.py astar tests/test_anytime.txt h2 1
+python game/run.py astar tests/test_anytime.txt h3 1
+
+python game/run.py astar tests/test_anytime2.txt h0 1
+python game/run.py astar tests/test_anytime2.txt h1 1
+python game/run.py astar tests/test_anytime2.txt h2 1
+python game/run.py astar tests/test_anytime2.txt h3 1
+
+
+python game/run.py astar tests/test1.txt h0 1.5
+python game/run.py astar tests/test1.txt h1 1.5
+python game/run.py astar tests/test1.txt h2 1.5
+python game/run.py astar tests/test1.txt h3 1.5
+
+python game/run.py astar tests/test2.txt h0 1.5
+python game/run.py astar tests/test2.txt h1 1.5
+python game/run.py astar tests/test2.txt h2 1.5
+python game/run.py astar tests/test2.txt h3 1.5
+
+python game/run.py astar tests/test3.txt h0 1.5
+python game/run.py astar tests/test3.txt h1 1.5
+python game/run.py astar tests/test3.txt h2 1.5
+python game/run.py astar tests/test3.txt h3 1.5
+
+python game/run.py astar tests/test4.txt h0 1.5
+python game/run.py astar tests/test4.txt h1 1.5
+python game/run.py astar tests/test4.txt h2 1.5
+python game/run.py astar tests/test4.txt h3 1.5
+
+python game/run.py astar tests/test5.txt h0 1.5
+python game/run.py astar tests/test5.txt h1 1.5
+python game/run.py astar tests/test5.txt h2 1.5
+python game/run.py astar tests/test5.txt h3 1.5
+
+python game/run.py astar tests/test6.txt h0 1.5
+python game/run.py astar tests/test6.txt h1 1.5
+python game/run.py astar tests/test6.txt h2 1.5
+python game/run.py astar tests/test6.txt h3 1.5
+
+python game/run.py astar tests/test7.txt h0 1.5
+python game/run.py astar tests/test7.txt h1 1.5
+python game/run.py astar tests/test7.txt h2 1.5
+python game/run.py astar tests/test7.txt h3 1.5
+
+python game/run.py astar tests/test_anytime.txt h0 1.5
+python game/run.py astar tests/test_anytime.txt h1 1.5
+python game/run.py astar tests/test_anytime.txt h2 1.5
+python game/run.py astar tests/test_anytime.txt h3 1.5
+
+python game/run.py astar tests/test_anytime2.txt h0 1.5
+python game/run.py astar tests/test_anytime2.txt h1 1.5
+python game/run.py astar tests/test_anytime2.txt h2 1.5
+python game/run.py astar tests/test_anytime2.txt h3 1.5
+
+python game/run.py astar tests/test1.txt h0 2
+python game/run.py astar tests/test1.txt h1 2
+python game/run.py astar tests/test1.txt h2 2
+python game/run.py astar tests/test1.txt h3 2
+
+python game/run.py astar tests/test2.txt h0 2
+python game/run.py astar tests/test2.txt h1 2
+python game/run.py astar tests/test2.txt h2 2
+python game/run.py astar tests/test2.txt h3 2
+
+python game/run.py astar tests/test3.txt h0 2
+python game/run.py astar tests/test3.txt h1 2
+python game/run.py astar tests/test3.txt h2 2
+python game/run.py astar tests/test3.txt h3 2
+
+python game/run.py astar tests/test4.txt h0 2
+python game/run.py astar tests/test4.txt h1 2
+python game/run.py astar tests/test4.txt h2 2
+python game/run.py astar tests/test4.txt h3 2
+
+python game/run.py astar tests/test5.txt h0 2
+python game/run.py astar tests/test5.txt h1 2
+python game/run.py astar tests/test5.txt h2 2
+python game/run.py astar tests/test5.txt h3 2
+
+python game/run.py astar tests/test6.txt h0 2
+python game/run.py astar tests/test6.txt h1 2
+python game/run.py astar tests/test6.txt h2 2
+python game/run.py astar tests/test6.txt h3 2
+
+python game/run.py astar tests/test7.txt h0 2
+python game/run.py astar tests/test7.txt h1 2
+python game/run.py astar tests/test7.txt h2 2
+python game/run.py astar tests/test7.txt h3 2
+
+python game/run.py astar tests/test_anytime.txt h0 2
+python game/run.py astar tests/test_anytime.txt h1 2
+python game/run.py astar tests/test_anytime.txt h2 2
+python game/run.py astar tests/test_anytime.txt h3 2
+
+python game/run.py astar tests/test_anytime2.txt h0 2
+python game/run.py astar tests/test_anytime2.txt h1 2
+python game/run.py astar tests/test_anytime2.txt h2 2
+python game/run.py astar tests/test_anytime2.txt h3 2
